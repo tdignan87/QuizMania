42b3210a4b6a2ef7e8f1d4181aa2d3a1ac019d02
Tom <tdignan87@gmail.com>
2020-03-28 20:04:53 +0000

    added emailjs script

--- assets/js/email.js
+++ assets/js/email.js
@@ -2,3 +2,5 @@ $("#navbar_register_btn").on("click", function(e) {
     e.preventDefault();
     $('#contactModal').modal('show');
 })
+
+document.getElementById('descript-txt').style.width = "300px";

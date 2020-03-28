74caa963394b492824492d03c23307d8392e8cff
Tom <tdignan87@gmail.com>
2020-03-28 20:07:07 +0000

    added comments to email.js file

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,6 +1,25 @@
+//jQuery for displaying modal on navbar
 $("#navbar_register_btn").on("click", function(e) {
     e.preventDefault();
     $('#contactModal').modal('show');
 })
 
+//JS for altering width of the description textbox.
 document.getElementById('descript-txt').style.width = "300px";
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value
+        })
+        .then(
+            function(response) {
+                console.log("SUCCESS", response);
+            },
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
+}

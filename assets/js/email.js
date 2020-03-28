3a0605f558f6efda04e36a89c2d00864f95d0aff
Tom <tdignan87@gmail.com>
2020-03-28 21:28:02 +0000

    tidied up email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,23 +1,26 @@
 //jQuery for displaying modal on navbar
 $("#navbar_register_btn").on("click", function(e) {
     e.preventDefault();
-    $('#contactModal').modal('show');
+    $("#contactModal").modal("show");
 })
 
 
 
-function sendMailMessage(contactForm) {
+function sendMail(contactForm) {
     emailjs.send("gmail", "emailQM", {
-            "from_name": contactForm.name.value,
-            "from_email": contactForm.from_email.value,
-            "recommendations": contactForm.recommendations.value
+            "from_name": contactForm.yourname.value,
+            "from_email": contactForm.youremail.value,
+            "recommendations": contactForm.yourrecomend.value
         })
         .then(
             function(response) {
                 console.log("SUCCESS", response);
+                document.getElementById("statusMsg").style.display = "block";
+                document.getElementById("statusMsg").innerHTML = "Sent Successfully";
+
             },
             function(error) {
-                console.log("FAILED", error);
+                document.getElementById("statusMsg").style.display = "block";
             }
         );
     return false; // To block from loading a new page

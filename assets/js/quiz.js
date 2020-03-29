9c29b79814625f164b1009ac38fbd79d20b7c3d5
Tom <tdignan87@gmail.com>
2020-03-29 18:42:32 +0100

    added for each loop for answers

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -49,18 +49,35 @@ startAgain = () => {
 
 
 getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign("/end.html");
+    }
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
     choices.forEach(choice => {
-        const number = choice.dataset['number'];
+        const number = choice.dataset["number"];
         choice.innerText = currentQuestion["choice" + number];
+    });
 
-    })
-
-
+    availableQuestions.splice(questionIndex, 1);
+    console.log(availableQuestions);
+    acceptingAnswers = true;
 };
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+        console.log(selectedAnswer);
+        getNewQuestion();
+    });
+});
 
 startAgain();

97623ea60d56a34cb2bcd99d87ac7e5c1cce34c5
Tom <tdignan87@gmail.com>
2020-03-30 20:52:43 +0100

    tidied up formatting

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,8 +1,7 @@
 const highScoresList = document.getElementById("finalScore");
 const resultScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-console.log(resultScores);
 
 highScores.map(score => {
-    console.log(`<li>${score.name}-${score.score}</li>`);
+    //   console.log(`<li>${score.name}-${score.score}</li>`);
 });

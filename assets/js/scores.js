6b5bc59ec3a9282d8ba8285c2f839b2bb2ab6e36
Tom <tdignan87@gmail.com>
2020-03-31 11:52:17 +0100

    removed top scores area

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,7 +1,7 @@
-const highScoresList = document.getElementById("finalScore");
-const resultScores = JSON.parse(localStorage.getItem("highScores")) || [];
+//const highScoresList = document.getElementById("finalScore");
+//const resultScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 
-highScores.map(score => {
-    //   console.log(`<li>${score.name}-${score.score}</li>`);
-});
+//highScores.map(score => {
+//   console.log(`<li>${score.name}-${score.score}</li>`);
+//});

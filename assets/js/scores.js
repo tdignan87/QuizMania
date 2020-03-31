e9c8fe97b6c18516b59ed9a4cd901f2524d28569
Tom <tdignan87@gmail.com>
2020-03-31 14:40:21 +0100

    removed js page

--- assets/js/scores.js
+++
@@ -1,7 +0,0 @@
-//const highScoresList = document.getElementById("finalScore");
-//const resultScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
-
-//highScores.map(score => {
-//   console.log(`<li>${score.name}-${score.score}</li>`);
-//});

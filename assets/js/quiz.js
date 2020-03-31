5a002d3ce9c966416db429d0c5e4db0726a0ebb7
Tom <tdignan87@gmail.com>
2020-03-31 11:51:57 +0100

    removed top stores for now due to time constraints

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -4,10 +4,6 @@ const questionCounterText = document.getElementById("questionCounter");
 const scoreText = document.getElementById("score");
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
-const finalScore = document.getElementById("finalScore");
-const mostRecentScore = localStorage.getItem("mostRecentScore");
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-finalScore.innerText = mostRecentScore;
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -15,7 +11,6 @@ let questionCounter = 0;
 let availableQuestions = [];
 
 
-console.log(choices);
 
 
 
@@ -26,7 +21,7 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
         return res.json();
     })
     .then(availableQuestions => {
-        console.log(availableQuestions.results);
+
         questions = availableQuestions.results.map(availableQuestions => {
             const formattedQuestion = {
                 question: availableQuestions.question
@@ -74,6 +69,7 @@ startAgain = () => {
 
 getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem("mostRecentScore", score);
         //go to the end page
         return window.location.assign("/index.html"); // needs changed here so it goes to results section of page.
     }
@@ -83,7 +79,7 @@ getNewQuestion = () => {
     // Store score in local storage
     // Template literal to show no questions out of how many.
     questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
-    localStorage.setItem("mostRecentScore", score);
+
 
     // make interger
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
@@ -123,7 +119,7 @@ choices.forEach(choice => {
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(answerToApply);
             getNewQuestion();
-        }, 1000); // wait for 1 second
+        }, 2000); // wait for 2 second
 
     });
 });
@@ -132,24 +128,3 @@ incrementScore = num => {
     score += num;
     scoreText.innerText = score;
 }
-
-
-username.addEventListener("keyup", () => {
-    saveScoreBtn.disabled = !username.value;
-})
-
-
-saveHighScore = (e) => {
-    e.preventDefault();
-}
-
-
-const scoreArray = {
-    score: Math.floor(Math.random() * 100),
-    name: username.value
-};
-highScores.push(score);
-highScores.sort((a, b) => {
-        return b.score - a.score;
-    }) // highScores.splice(5);
-localStorage.setItem("highScores", JSON.stringify(highScores));

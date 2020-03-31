a9713fc8eca8b0331e5d444daed74a1827178da6
Tom <tdignan87@gmail.com>
2020-03-31 11:57:59 +0100

    Removed scores navbar

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -54,7 +54,7 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=mul
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 const MAX_HIGH_SCORES = 5;
 
 // Start again function to reset values

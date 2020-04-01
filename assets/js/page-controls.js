5dd85352b2f258cda2bd5a1f9aa366dce746b2a3
Tom <tdignan87@gmail.com>
2020-04-01 23:52:46 +0100

    Finished off hiding controls

--- assets/js/page-controls.js
+++ assets/js/page-controls.js
@@ -10,11 +10,16 @@ window.onload = function() {
 };
 
 /**
- * @function called showQuestions will show the questions container grid when the play button is pressed.
+ * @function called showQuestions will show the questions container grid when the play button is pressed. 
+ * The jumbotron display will also not be visible.
  */
 
 let showQuestions = function() {
     document.getElementById("question_grid").style.display = "block";
     document.getElementById("score_grid").style.display = "block";
+
+    document.getElementById("options-container-choices").style.display = "none";
+    document.getElementById("jumbo-picture-main").style.display = "none";
+
 
 };

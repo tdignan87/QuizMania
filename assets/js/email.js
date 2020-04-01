73a006b9d96cb833719c53caeedc94e258cc72f2
Tom <tdignan87@gmail.com>
2020-04-01 10:55:50 +0100

    Added function names to emailjs

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,27 +1,16 @@
-$("#navbar_register_btn").on("click", function(e) {
-    e.preventDefault();
-    $("#contactModal").modal("show");
-})
-
-
-
-function sendMail(contactForm) {
+function sendMail(contactUs) {
     emailjs.send("gmail", "emailQM", {
-            "from_name": contactForm.yourname.value,
-            "from_email": contactForm.youremail.value,
-            "recommendations": contactForm.yourrecomend.value
+            "from_name": contactUs.nameInputBox.value,
+            "from_email": contactUs.emailInputBox.value,
+            "recommendations": contactUs.recommendationInput.value
         })
         .then(
             function(response) {
                 console.log("SUCCESS", response);
-                document.getElementById("statusMsg").style.display = "block";
-                document.getElementById("statusMsg").innerHTML = "Sent Successfully";
-
             },
             function(error) {
-                console.log("SUCCESS", response);
-                document.getElementById("statusMsg").style.display = "block";
-                document.getElementById("statusMsg").innerHTML = "Failed To Send";
+                console.log("FAILED", response);
+
             }
         );
     return false; // To block from loading a new page

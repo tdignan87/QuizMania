0593b37465034f87b42c7b0249b719b3a506ddc4
Tom <tdignan87@gmail.com>
2020-04-01 23:30:00 +0100

    changed style display to visible on click event for play btn

--- assets/js/page-controls.js
+++ assets/js/page-controls.js
@@ -6,13 +6,14 @@ window.onload = function() {
     document.getElementById("question_grid").style.display = "none";
     document.getElementById("score_grid").style.display = "none";
     document.getElementById("contactus_grid").style.display = "none";
-    document.getElementById("email-submit-btn").style.display = "none";
+
 };
 
 /**
  * @function called showQuestions will show the questions container grid when the play button is pressed.
  */
 
-let showQuestions = function() {
-    document.getElementById("question_grid").style.display = "show";
-}
+var showQuestions = function() {
+    document.getElementById("question_grid").style.display = "visible";
+    console.log("hello world");
+};

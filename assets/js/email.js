457c86403882fbeb6ab67356780e5d30c99534d6
Tom <tdignan87@gmail.com>
2020-04-01 11:29:36 +0100

    still to debug

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,19 +1,19 @@
-function sendMail(contactUs) {
+function sendMail(contactForm) {
     emailjs.send("gmail", "emailQM", {
-            "from_name": contactUs.nameInputBox.value,
-            "from_email": contactUs.emailInputBox.value,
-            "recommendations": contactUs.recommendationInput.value
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "recommendations": contactForm.recommendationInput.value
         })
         .then(
             function(response) {
                 console.log("SUCCESS", response);
+
             },
             function(error) {
                 console.log("FAILED", response);
+
 
             }
         );
     return false; // To block from loading a new page
 }
-
-console.log("hello world");

30b7a91b0ea680af57e502795000689d9cf231d0
Tom <tdignan87@gmail.com>
2020-04-02 20:13:15 +0100

    Added API key

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,3 +1,22 @@
-function getQuestionsAnswers() {
-    fetch("https://opentdb.com/api.php?amount=10&category=9")
-};
+const tenQuestions = 10;
+const twentyQuestions = 20;
+const thirtyQuestions = 30;
+const difficultyEasy = "easy";
+const difficultyMedium = "medium";
+const difficultyHard = "hard";
+
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+    .then(function(response) {
+        console.log(response);
+        if (response.status == 200) {
+            console.log("connected successfully");
+            let x = document.getElementById("dropdown-options").value;
+            console.log(x);
+
+
+        } else {
+            console.log("failed to connect");
+
+
+        }
+    })

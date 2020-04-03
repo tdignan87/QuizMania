ba6cb8981fdc9d1039a760d22bb3e22f0db4aaf6
Tom <tdignan87@gmail.com>
2020-04-03 12:18:27 +0100

    Fetch API on load

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,21 +1,12 @@
-const tenQuestions = 10;
-const twentyQuestions = 20;
-const thirtyQuestions = 30;
-const difficultyEasy = "easy";
-const difficultyMedium = "medium";
-const difficultyHard = "hard";
+/** function loads on Index load event. Using function to fetch the open trivia API.
+ ** then if we get a response, we are converting it to JSON format and then logging the raw data into the console.
+ */
 
-fetch("https://opentdb.com/api.php?amount=" + tenQuestions + "&category=9&difficulty=" + difficultyEasy + "&type=multiple")
-    .then(function(response) {
-        console.log(response);
-        if (response.status == 200) {
-            console.log("connected successfully");
+window.onload = function retrieveQuestions() {
+    fetch(`https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`
 
-
-
-        } else {
-            console.log("failed to connect");
-
-
-        }
-    });
+        ).then(response => response.json())
+        .then(rawData => {
+            console.log(rawData.results);
+        })
+}

ae68bb4fdc877b7459f8e60c52a4344d4a74fe29
Tom <tdignan87@gmail.com>
2020-04-08 18:17:32 +0100

    Changed const and Lets

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,18 +1,10 @@
 const noOfQuestions = [10, 20, 30];
 const difficultySetting = ["easy", "medium", "hard"];
-const choiceOptions = ["A", "B", "C", "D"];
-const questionCounter = document.getElementById("questionCount");
+const category = null;
+let difficulty = null;
+let questions = null;
 let allQuestions = [];
-let correctAnswer = [];
-let incorrectAnswers = [];
-let score = 0;
-let counter = 0;
-let acceptingInput = false;
-let availableQuestions = [];
-let catChoice = document.getElementById("dropdown-choices-category");
-let difficultyOptions = document.getElementById("dropdown-choices-difficulty");
-let questionOptions = document.getElementById("dropdown-choices-questions");
-let answers = Array.from(document.getElementsByClassName("choice-answer"));
+let userScore = 0;
 
 /** Fetches API and converts to JSON format.
  * 
@@ -59,7 +51,7 @@ function populateAnswers(answers) {
     answers.forEach(function(item, index) {
         $(`#available-answers`).append(`<div class="col-sm">
         <p class="choice-options" >${choiceOptions[index]}</p>
-         <p class="choice-answer" id="answer-opt"  data-number="${index +1}">${item}</p>
+         <p class="choice-answer" id="answer-opt" onclick="verifyAnswer()"  data-number="${index +1}">${item}</p>
      </div>`)
 
     });
@@ -73,51 +65,47 @@ function populateAnswers(answers) {
 }
 /** Retrieves category list from API and passes to dropdown on homepage.
  */
-$(document).ready(function generateCategories() {
+(document).ready(function generateCategories() { // this is your code
     fetch(`https://opentdb.com/api_category.php`)
         .then(res => res.json())
         .then(data => {
             data.trivia_categories.forEach(category => {
-                $("#dropdown-choices-category").append(`<option value="${category.id}">${category.name}</option>`)
+                $("#dropdown-choices-category").append(`<option value="${category.id}">${category.name}</option>`);
             });
-
-        })
-    $(document).ready(function questionsAmount() {
-        $.each(noOfQuestions, function(val, text) {
-            $('#dropdown-choices-questions').append($(`<option>${text}</option>`))
         });
-    })
+    $.each(noOfQuestions, function(val, text) {
+        $('#dropdown-choices-questions').append($(`<option>${text}</option>`));
+    });
 
-    $(document).ready(function generateDifficulty() {
-        $.each(difficultySetting, function(val, text) {
-            $('#dropdown-choices-difficulty').append($(`<option>${text}</option>`))
-        });
-    })
+    $.each(difficultySetting, function(val, text) {
+        $('#dropdown-choices-difficulty').append($(`<option>${text}</option>`));
+    });
+});
 
-    /** Play submit function IF statements to ensure criteria is selected before game will allow to play. All options must be selected before questions will be generated.
-     */
+/** Play submit function IF statements to ensure criteria is selected before game will allow to play. All options must be selected before questions will be generated.
+ */
 
-    $("#play-submit-btn").click(function() {
-        category = catChoice.options[catChoice.selectedIndex].id;
-        difficulty = difficultyOptions.options[difficultyOptions.selectedIndex].value;
-        questions = questionOptions.options[questionOptions.selectedIndex].value;
+$("#play-submit-btn").click(function() {
+category = catChoice.options[catChoice.selectedIndex].id;
+difficulty = difficultyOptions.options[difficultyOptions.selectedIndex].value;
+questions = questionOptions.options[questionOptions.selectedIndex].value;
 
-        if (($(`#dropdown-choices-difficulty option:selected`).index() > 0) && ($(`#dropdown-choices-category option:selected`).index() > 0) && ($(`#dropdown-choices-questions option:selected`).index() > 0)) {
+if (($(`#dropdown-choices-difficulty option:selected`).index() > 0) && ($(`#dropdown-choices-category option:selected`).index() > 0) && ($(`#dropdown-choices-questions option:selected`).index() > 0)) {
 
-            document.getElementById("question_grid").style.display = "block";
-            document.getElementById("score_grid").style.display = "block";
-            document.getElementById("options-container-choices").style.display = "none";
-            document.getElementById("jumbo-picture-main").style.display = "none";
-            document.getElementById("play-submit-btn").style.display = "none";
-            getQuestions();
+    document.getElementById("question_grid").style.display = "block";
+    document.getElementById("score_grid").style.display = "block";
+    document.getElementById("options-container-choices").style.display = "none";
+    document.getElementById("jumbo-picture-main").style.display = "none";
+    document.getElementById("play-submit-btn").style.display = "none";
+    getQuestions();
 
 
-        } else {
-            document.getElementById("main-status").style.display = "block";
+} else {
+    document.getElementById("main-status").style.display = "block";
 
-        }
+}
 
-    })
+})
 
 })
 
